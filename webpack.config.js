964c4b3cc6846179c1b838cd282aeb1aa501824c
Vlad <vlad.markevichh@gmail.com>
2021-12-26 05:11:03 +0200

    add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,137 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
+const CupyWebpackPlugin    = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin  = require('terser-webpack-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+    const loaders = [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+        },
+        'css-loader'
+    ]
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders;
+}
+
+const checkTarget = () => isDev ? 'web' : 'browserslist'
+
+module.exports = {
+    target: checkTarget(),
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './scripts/index.js'],
+        analytics: './scripts/analytics.js'
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: optimization(),
+
+    devServer: {
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        inline: true,
+        index: path.resolve(__dirname, 'index.html'),
+        port: 5500,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CupyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src/assets/favicon/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
